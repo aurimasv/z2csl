5469d5fa6855fdd732a658b9ed33dfb54392c53e
aurimasv <aurimas.dev@gmail.com>
2012-05-20 02:48:50 -0500

    Z2CSL: initial commit. Zotero extension that creates type and field mappings between Zotero and CSL items.

---
+++ content/overlay.js
@@ -0,0 +1,144 @@
+var z2csl = {
+	exportMappings: function() {
+		//load utilities.js so we can fetch the CSL mappings
+		var context = { Zotero: {} };
+		Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+			.getService(Components.interfaces.mozIJSSubScriptLoader)
+			.loadSubScript("chrome://zotero/content/xpcom/utilities.js", context);
+		var cslTypeMap = context.CSL_TYPE_MAPPINGS;
+		var cslFieldMap = context.CSL_TEXT_MAPPINGS;
+		var cslDateMap = context.CSL_DATE_MAPPINGS;
+		var cslCreatorMap = context.CSL_NAMES_MAPPINGS;
+
+		var map = {name:'map', childNodes:[] };
+
+		var zoteroTypes = Zotero.ItemTypes.getTypes();
+		var fields, type, creatorNodes, creators;
+		var nodes = [];
+
+		for(var i=0, n=zoteroTypes.length; i<n; i++) {
+		  type = {name:'typeMap', attributes:{zType:zoteroTypes[i].name, cslType:cslTypeMap[zoteroTypes[i].name]}, childNodes:[]};
+
+		  fields = Zotero.ItemFields.getItemTypeFields(zoteroTypes[i].id);
+		  for(var j=0, m=fields.length; j<m; j++) {
+	      type.childNodes.push({name:'field', attributes:{ value:Zotero.ItemFields.getName(fields[j]) } });
+      }
+      //add valid creator types
+      creators = Zotero.CreatorTypes.getTypesForItemType(zoteroTypes[i].id);
+      if(creators.length) {
+      	creatorNodes = {name:'field', attributes:{ value:'creator'}, childNodes: []};
+      	for(var j=0, m=creators.length; j<m; j++) {
+      		creatorNodes.childNodes.push({name:'creatorType', attributes:{ value:creators[j].name } });
+      	}
+      	type.childNodes.push(creatorNodes);
+      }
+
+      nodes.push(type);
+		}
+		map.childNodes.push({name:'zTypes', childNodes:nodes}) ;
+
+		nodes = [];
+		var f;
+		for(f in cslFieldMap) {
+	    for(var i=0, n=cslFieldMap[f].length; i<n; i++) {
+	      nodes.push({name:'fieldMap', attributes:{zField: cslFieldMap[f][i], cslField: f}});
+	    }
+		}
+
+		//add dates
+		for(f in cslDateMap) {
+      nodes.push({name:'fieldMap', attributes:{zField: cslDateMap[f], cslField: f}});
+		}
+		map.childNodes.push({name:'cslFieldMap', childNodes:nodes});
+
+		//add csl creator map
+		nodes = [];
+		for(f in cslCreatorMap) {
+			nodes.push({name:'creatorMap', attributes:{zCreator: f, cslCreator: cslCreatorMap[f]}});
+		}
+		map.childNodes.push({name:'cslCreatorMap', childNodes:nodes});
+
+		z2csl.writeFile(z2csl.jsonToXML(map));
+	},
+
+	writeFile: function(data) {
+		//output XML data
+		Components.utils.import("resource://gre/modules/NetUtil.jsm");  
+		Components.utils.import("resource://gre/modules/FileUtils.jsm"); 
+
+		var nsIFilePicker = Components.interfaces.nsIFilePicker;
+		var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+		fp.init(window, "Select a file to save to", nsIFilePicker.modeSave);
+
+		fp.appendFilters(nsIFilePicker.filterXML)
+		fp.appendFilters(nsIFilePicker.filterAll);
+
+		fp.defaultExtension = 'xml';
+		fp.defaultString = 'typeMap';
+
+		var res = fp.show();
+
+		if (res != nsIFilePicker.returnCancel){
+		  var file = fp.file;
+		  var ostream = FileUtils.openSafeFileOutputStream(file)  
+
+		  var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].  
+		                 createInstance(Components.interfaces.nsIScriptableUnicodeConverter);  
+		  converter.charset = "UTF-8";  
+		  var istream = converter.convertToInputStream(data);
+		  NetUtil.asyncCopy(istream, ostream);
+		}
+	},
+
+	jsonToXML: function(obj, indent) {
+		if(typeof(attributes) != 'object') {
+			attributes = {};
+		}
+
+		var indentDelim = '  ';		//two spaces
+
+		var xml = '';
+		//top level
+		if(typeof(indent) == 'undefined') {
+			xml += '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="typeMap.xsl"?>\n\n';
+			indent = '';
+		} else {
+			indent += indentDelim;
+		}
+
+		xml += indent + '<' + obj.name;
+		var attr;
+		if(obj.attributes) {
+			for(attr in obj.attributes) {
+				xml += ' ' + attr + '="' + z2csl.stringify(obj.attributes[attr]) + '"';
+			}
+		}
+
+		if(obj.value || obj.childNodes) {
+			xml += '>';
+
+			if(obj.value) {
+				xml += z2csl.stringify(obj.value);
+			} else if(obj.childNodes) {
+				xml += '\n';
+				for(var i=0, n=obj.childNodes.length; i<n; i++) {
+					xml += z2csl.jsonToXML(obj.childNodes[i], indent) + '\n';
+				}
+				xml += indent;
+			}
+			xml += '</' + obj.name + '>';
+		} else {
+			xml += ' />';
+		}
+
+		return xml;
+	},
+
+	stringify: function(obj) {
+		if(typeof(obj) == 'undefined' || typeof(obj) == 'null') {
+			return '';
+		} else {
+			return '' + obj;
+		}
+	}
+};
---
+++ typeMap.xsl
@@ -0,0 +1,60 @@
+﻿<?xml version="1.0"?>
+
+<xsl:stylesheet version="1.0"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/">
+  <html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="typeMap.css" />
+  </head>
+  <body>
+    <h2><a name="toc">Zotero Item Types</a></h2>
+    <table>
+      <thead>
+          <tr><th>Zotero type</th><th>CSL type</th></tr>
+      </thead>
+      <tbody>
+      <xsl:for-each select="map/zTypes/typeMap">
+        <xsl:sort select="@zType"/>
+        <tr>
+          <td><a href="#map-{@zType}"><xsl:value-of select="@zType"/></a></td>
+          <td><xsl:value-of select="@cslType"/></td>
+        </tr>
+      </xsl:for-each>
+      </tbody>
+    </table>
+    <xsl:for-each select="map/zTypes/typeMap">
+    <xsl:sort select="@zType"/>
+      <hr />
+      (<a href="#toc">top</a>)
+      <h3><a name="map-{@zType}"><xsl:value-of select="@zType"/> &#x2192; <xsl:value-of select="@cslType"/></a></h3>
+      <table>
+        <thead>
+            <tr><th>Zotero field</th><th>CSL field</th></tr>
+        </thead>
+        <tbody>
+        <xsl:for-each select="field">
+        <xsl:sort select="@value"/>
+          <tr>
+            <td><xsl:value-of select="@value"/></td>
+            <td><xsl:value-of select="/map/cslFieldMap/fieldMap[@zField=current()/@value]/@cslField"/></td>
+          </tr>
+          <xsl:if test="@value = 'creator'">
+            <xsl:for-each select="creatorType">
+            <xsl:sort select="@value"/>
+              <tr>
+                <td class="zSubType"><xsl:value-of select="@value"/></td>
+                <td class="zSubType"><xsl:value-of select="/map/cslCreatorMap/creatorMap[@zCreator=current()/@value]/@cslCreator"/></td>
+              </tr>
+            </xsl:for-each>
+          </xsl:if>
+        </xsl:for-each>
+        </tbody>
+      </table>
+    </xsl:for-each>
+  </body>
+  </html>
+</xsl:template>
+
+</xsl:stylesheet>
