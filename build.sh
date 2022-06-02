a71ff1344cd57ca917e711b8c0fce3934d8fc198
Sebastian Karcher <karcher@u.northwestern.edu>
2022-06-01 21:43:23 -0400

    Cleaning up master to turn it into deployed branch

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-rm -f build/z2csl.xpi
-zip -r  build/z2csl.xpi \
-	chrome.manifest \
-	install.rdf \
-	content/
--- content/include.js
+++
@@ -1,6 +0,0 @@
-// Only create main object once
-if (!Zotero.Z2CSL) {
-	const loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-					.getService(Components.interfaces.mozIJSSubScriptLoader);
-	loader.loadSubScript("chrome://z2csl/content/z2csl.js");
-}
